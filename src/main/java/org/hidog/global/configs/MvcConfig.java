bfa03ce50bcb0d1552548c31b5a41ff5c6de74a8
jeongsuri <jeongsuri96@gmail.com>
2024-08-07 14:27:17 +0900

    feat: redis 설정
    세션공유를 위한 redis 구성 설정

--- src/main/java/org/hidog/global/configs/MvcConfig.java
+++ src/main/java/org/hidog/global/configs/MvcConfig.java
@@ -1,10 +1,8 @@
 package org.hidog.global.configs;
 
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableDiscoveryClient
 public class MvcConfig implements WebMvcConfigurer {
 }
